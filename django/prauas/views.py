dc33330b65ecb9dc5201d116a090622c974e8f55
Filipus Dani <75106235+filipusdani@users.noreply.github.com>
2023-04-28 21:50:10 +0700

    Added ML

--- django/prauas/views.py
+++ django/prauas/views.py
@@ -4,11 +4,13 @@ from django.core.serializers import serialize
 
 import paho.mqtt.client as mqtt
 from .mqtt import *
+from .ml import *
 
 def home(req):
     return render(req, "index.html")
 
 def api(req):
+    milking_machine,slaughter_machine,feeding_machine,gramasi_karbo,sprayer,pemberi_pupuk,aircon,hasil_penjualan,jumlah_kursi,output_produk,hasil_tanam,proyeksi_keuntungan,nilai_pabrik = hitung_ml()
     data = {
         "Temperature": serialize("json", list(reversed(Temperature.objects.all()))),
         "Conductivity": serialize("json", list(reversed(Conductivity.objects.all()))),
@@ -37,8 +39,67 @@ def api(req):
         "Occupancy": serialize("json", list(reversed(Occupancy.objects.all()))),
         "Audio": serialize("json", list(reversed(Audio.objects.all()))),
         "Sentimen": serialize("json", list(reversed(Sentimen.objects.all()))),
+        "milking_machine": milking_machine,
+        "slaughter_machine": slaughter_machine,
+        "feeding_machine": feeding_machine,
+        "gramasi_karbo": gramasi_karbo,
+        "sprayer": sprayer,
+        "pemberi_pupuk": pemberi_pupuk,
+        "aircon": aircon,
+        "hasil_penjualan": hasil_penjualan,
+        "jumlah_kursi": jumlah_kursi,
+        "output_produk": output_produk,
+        "hasil_tanam": hasil_tanam,
+        "proyeksi_keuntungan": proyeksi_keuntungan,
     }
     return JsonResponse(data)
+
+def hitung_ml():
+    aTemperature = (list(reversed(Temperature.objects.all())))[0].value
+    aConductivity = (list(reversed(Conductivity.objects.all())))[0].value
+    aFlow = (list(reversed(Flow.objects.all())))[0].value
+    aCO = (list(reversed(CO.objects.all())))[0].value
+    aAmonia = (list(reversed(Amonia.objects.all())))[0].value
+    aPressure = (list(reversed(Pressure.objects.all())))[0].value
+    aO2 = (list(reversed(O2.objects.all())))[0].value
+    aCO2 = (list(reversed(CO2.objects.all())))[0].value
+    aWaterlevel = (list(reversed(Waterlevel.objects.all())))[0].value
+    milking_machine = hitung_milking_machine(aTemperature,aConductivity,aFlow)
+    slaughter_machine = hitung_slaughter_machine(aCO,aAmonia,aPressure)
+    feeding_machine = hitung_feeding_machine(aO2,aCO2,aWaterlevel)
+
+    aMoisture = (list(reversed(Moisture.objects.all())))[0].value
+    aPH = (list(reversed(PH.objects.all())))[0].value
+    aSunlight = (list(reversed(Sunlight.objects.all())))[0].value
+    aHumidity = (list(reversed(Humidity.objects.all())))[0].value
+    aAnemometer = (list(reversed(Anemometer.objects.all())))[0].value
+    aUV = (list(reversed(UV.objects.all())))[0].value
+    aRadiation = (list(reversed(Radiation.objects.all())))[0].value
+    aBerat = (list(reversed(Berat.objects.all())))[0].value
+    aColor = (list(reversed(Color.objects.all())))[0].value
+    gramasi_karbo = hitung_gramasi_karbo(aMoisture,aPH,aSunlight)
+    sprayer = hitung_sprayer(aHumidity,aAnemometer,aUV)
+    pemberi_pupuk = hitung_pemberi_pupuk(aRadiation,aBerat,aColor)
+
+    aBarometer = (list(reversed(Barometer.objects.all())))[0].value
+    aWind = (list(reversed(Wind.objects.all())))[0].value
+    aVisibility = (list(reversed(Visibility.objects.all())))[0].value
+    aStock = (list(reversed(Stock.objects.all())))[0].value
+    aSellingrate = (list(reversed(Sellingrate.objects.all())))[0].value
+    aSatisfaction = (list(reversed(Satisfaction.objects.all())))[0].value
+    aOccupancy = (list(reversed(Occupancy.objects.all())))[0].value
+    aAudio = (list(reversed(Audio.objects.all())))[0].value
+    aSentimen = (list(reversed(Sentimen.objects.all())))[0].value
+    aircon = hitung_aircon(aBarometer,aWind,aVisibility)
+    hasil_penjualan = hitung_hasil_penjualan(aStock,aSellingrate,aSatisfaction)
+    jumlah_kursi = hitung_jumlah_kursi(aOccupancy,aAudio,aSentimen)
+
+    output_produk = hitung_output_produk(milking_machine,slaughter_machine,feeding_machine)
+    hasil_tanam = hitung_hasil_tanam(gramasi_karbo,sprayer,pemberi_pupuk)
+    proyeksi_keuntungan = hitung_proyeksi_keuntungan(aircon,hasil_penjualan,jumlah_kursi)
+
+    nilai_pabrik = hitung_nilai_pabrik(output_produk,hasil_tanam,proyeksi_keuntungan)
+    return milking_machine,slaughter_machine,feeding_machine,gramasi_karbo,sprayer,pemberi_pupuk,aircon,hasil_penjualan,jumlah_kursi,output_produk,hasil_tanam,proyeksi_keuntungan,nilai_pabrik
 
 client = mqtt.Client("prauas")
 client.message_callback_add('prauas/p1/temperature', on_message_temperature)
